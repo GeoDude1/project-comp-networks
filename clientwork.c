79d8611aefd621e2d710eafe29f9c60703451b90
Gerardo Aldana <47541281+GeoDude1@users.noreply.github.com>
2020-04-15 18:55:06 -0700

    Add files via upload

--- clientwork.c
+++ clientwork.c
@@ -11,6 +11,7 @@
 
 int main(int argc, char *argv[]) {
 	char buffer[1024]; //might need to change because our json file has more data
+	char body[200];
 
 	struct json_object *parsed_json; //structure that holds parsed JSON
 	//stores rest of fields of the JSON file
@@ -40,6 +41,7 @@ int main(int argc, char *argv[]) {
 	json_object_object_get_ex(parsed_json, "Number_UDP_Packets", &Number_UDP_Packets);
 	json_object_object_get_ex(parsed_json, "TTL_UDP_Packets", &TTL_UDP_Packets);
 
+	/*
 	printf("Server_IP_Address: %s\n", json_object_get_string(Server_IP_Address));
 	printf("Source_Port_Number_UDP: %s\n", json_object_get_string(Source_Port_Number_UDP));
 	printf("Destination_Port_Number_TCP_Head: %s\n", json_object_get_string(Destination_Port_Number_TCP_Head));
@@ -49,4 +51,28 @@ int main(int argc, char *argv[]) {
 	printf("Inter_Measurement_Time: %s\n", json_object_get_string(Inter_Measurement_Time));
 	printf("Number_UDP_Packets: %d\n", json_object_get_int(Number_UDP_Packets));
 	printf("TTL_UDP_Packets: %d\n", json_object_get_int(TTL_UDP_Packets));
+	*/
+
+	char *Server_IP_Address2 = json_object_get_string(Server_IP_Address);
+	printf("Sever IP: %s\n", Server_IP_Address2);
+	char *Source_Port_Number_UDP = json_object_get_string(Source_Port_Number_UDP);
+	char *Destination_Port_Number_TCP_Head = json_object_get_string(Destination_Port_Number_TCP_Head);
+	char *Destination_Port_Number_TCP_Tail = json_object_get_string(Destination_Port_Number_TCP_Tail);
+	char *Port_Number_TCP = json_object_get_string(Port_Number_TCP);
+	char *Size_UDP_Payload = json_object_get_string(Size_UDP_Payload);
+	char *Inter_Measurement_Time = json_object_get_string(Inter_Measurement_Time);
+	char *Number_UDP_Packets = json_object_get_string(Number_UDP_Packets);
+	char *TTL_UDP_Packets = json_object_get_string(TTL_UDP_Packets);
+
+
+
+	//where connection to the socket begins
+	int sockfd;
+	struct sockaddr_in serv_addr;
+	char buffer[1024];
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr("10.0.0.249");
+	serv_addr.sin_port = htons(8765);
 }
